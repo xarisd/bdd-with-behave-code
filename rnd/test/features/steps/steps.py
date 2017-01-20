1a29d8eed3074cbd3464e02f1b42d8da32c9f9d7
xarisd <xaris.dimitriou@gmail.com>
2017-01-20 18:07:08 +0200

    Add example test code

---
+++ rnd/test/features/steps/steps.py
@@ -0,0 +1,11 @@
+@given(u'we have behave installed')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given we have behave installed')
+
+@when(u'we implement a test')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When we implement a test')
+
+@then(u'behave will test it for us!')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then behave will test it for us!')
